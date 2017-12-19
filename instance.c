0b088a3b23ef0d23fea03524364c7a475a970aed
Gchevallier45 <chevallierguillaume45@gmail.com>
2017-12-19 15:50:16 +0100

    Instance 1.0.2

--- instance.c
+++ instance.c
@@ -8,18 +8,33 @@
 Instance ** Instance_createInstances(Instance *instanceStructure[30], FILE *instanceFile){
 
     char *line;
-    int numberLine = 0;
-    int instanceIndice = 0;
-    while((line = readLine(instanceFile)) != NULL){
-        //printf("%s",line);
-        instanceStructure[instanceIndice]=malloc(sizeof(Instance));
-        if(numberLine == 0){
-            char *pEnd;
-            //instanceReturned[instanceIndice]->objectNb = (int)strtol(line,pEnd,10);
-            numberLine++;
-        }
+
+    //On saute les 4 premières lignes du fichier
+    for(int j=0; j<2; j++){
+        line=readLine(instanceFile);
         free(line);
-        break;
+    }
+
+    for(int i=0; i<30; i++){
+        instanceStructure[i]=malloc(sizeof(Instance));
+
+        //On saute les deux lignes qui séparent chaque instance
+        for(int j=0; j<2; j++){
+            line=readLine(instanceFile);
+            free(line);
+        }
+
+        //On lit les informations de l'instance
+        line=readLine(instanceFile); //Ligne qui contient le nombre d’objet N, le nombre de dimension M, les valeurs connues de solutions trouvée
+        printf("%s\n",line);
+
+        //if(numberLine == 0){
+            //char *pEnd;
+            //instanceReturned[instanceIndice]->objectNb = (int)strtol(line,pEnd,10);
+            //numberLine++;
+        //}
+        free(line);
+        i=30;
     }
     return NULL;
 }
