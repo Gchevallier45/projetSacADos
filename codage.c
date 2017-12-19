f17d7c352e9a0a75033c1684568f1e4968b30b08
Rgoudon <robin.goudon@wanadoo.fr>
2017-12-19 18:03:03 +0100

    ajout de fcts vides dans codage

--- codage.c
+++ codage.c
@@ -4,8 +4,33 @@
 typedef struct _codageDirect{
 
     int* sac;
-    int solution;
+    int resultat;
     int faisabilite;
 
 
 }codageDirect;
+
+int directResultat(int* tab, int* objet){
+
+
+
+    return 1;
+}
+
+int directFaisable(int* tab){
+
+    return 1;
+}
+
+
+void affSoluce(int solu){
+
+
+}
+
+void writeSoluce(int solu){
+
+
+}
+
+
--- codage.h
+++ codage.h
@@ -1,6 +1,9 @@
 #ifndef CODAGE_H_INCLUDED
 #define CODAGE_H_INCLUDED
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #endif // CODAGE_H_INCLUDED
