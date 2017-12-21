71bc0cb43cbe54122cbffc4fff0db85fa4848de3
Gchevallier45 <chevallierguillaume45@gmail.com>
2017-12-21 09:55:41 +0100

    Creation des fonctions de base de la structure ListeObjets

---
+++ heuristique.c
@@ -0,0 +1,12 @@
+#include "heuristique.h"
+
+ListeObjets * ListeObjets_initCreer(){
+    ListeObjets * tmp = malloc(sizeof(ListeObjets));
+    tmp->objet = 0;
+    tmp->suivant = NULL;
+    return tmp;
+}
+
+ListeObjets * ListeObjets_videDetruire(ListeObjets * listeObjets){
+
+}
--- heuristique.h
+++ heuristique.h
@@ -1,6 +1,12 @@
 #ifndef HEURISTIQUE_H_INCLUDED
 #define HEURISTIQUE_H_INCLUDED
 
+typedef struct _ListeObjets{
+    int objet;
+    struct _ListeObjet *suivant;
+} ListeObjets;
 
+ListeObjets * ListeObjets_initCreer();
+ListeObjets * ListeObjets_videDetruire(ListeObjets * listeObjets);
 
 #endif // HEURISTIQUE_H_INCLUDED
