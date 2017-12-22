f280056e5cacb3992a10c51142dff71b5fa3046d
Rgoudon <robin.goudon@wanadoo.fr>
2017-12-22 12:24:38 +0100

    je sais plus

--- heuristique.c
+++ heuristique.c
@@ -16,22 +16,25 @@ void ListeObjets_videDetruire(ListeObjets * listeObjets){
     free(listeObjets);
 }
 
+
 int * randPick(Instance * instance){
 
-    int* intermediaire;
-    int* tabAlea;
+    int* intermediaire; //tableau contenant les indices des objets pour pouvoir les retrouver par la suite
+    int* tabAlea;       //tableau qui est rempli aléatoirement avec les valeurs de intermediaire
     int i;
 
     intermediaire = (int*)malloc( (instance->objetNb) * sizeof(int));
     tabAlea = (int*)malloc( (instance->objetNb) * sizeof(int));
+
+    //remplissage des indices des objets dans intermediaire
     for(i=0; i < instance->objetNb; i++){
         intermediaire[i] = i;
     }
 
+    //remplissage aleatoire de tabAlea
     for(i = 0; i < instance->objetNb; i++){
-        tabAlea[i] = intermediaire[rand() % (instance->objetNb - i)];
-        retraitElemTab(&intermediaire,instance->objetNb-i,tabAlea[i]);
-
+        tabAlea[i] = intermediaire[rand() % (instance->objetNb - i)]; //prend un element aleatoire de intermediaire
+        retraitElemTab(&intermediaire,instance->objetNb-i,tabAlea[i]);//retire de intermediaire l'élement qui a été pris dans le tableau
     }
 
     return tabAlea;
