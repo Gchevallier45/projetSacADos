bac212fbff7afef4186eb91fe156d06052bf9890
Gchevallier45 <chevallierguillaume45@gmail.com>
2018-01-10 10:20:29 +0100

    meta tabou

--- metaheuristique.c
+++ metaheuristique.c
@@ -173,14 +173,13 @@ void metaTabouIndirecte(int* tab, Instance *instance){
  * @param instance l'instance à utiliser pour générer la solution
  * Préconditions : tab non nul, et d'une taille correspondant au nombre d'objets de l'instance
  */
-void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax){
+void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax, int aspi, int tailleListe){
     int *solutionCourante = malloc(instance->objetNb*sizeof(int)); //En codage direct
     int *solutionBest = malloc(instance->objetNb*sizeof(int)); //En codage direct
     int *solutionVoisine = malloc(instance->objetNb*sizeof(int)); //En codage direct
     int *solutionBestVoisine = malloc(instance->objetNb*sizeof(int)); //En codage direct
     int nbMouvements = 0;
     int **listeMouvements = malloc(nbMouvements*sizeof(int*));
-    int mouvementUtil[2];
 
     Direct(solutionCourante, instance,3); //On génère la solution de départ avec l'algo d'ordonnancement le + performant
     memcpy(solutionBest,solutionCourante,instance->objetNb*sizeof(int)); //Copie de solutionCourante dans solutionBest
@@ -192,22 +191,46 @@ void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax){
     int fcourant = fbest;
 
     while(nbIte < nbIteMax){
+        int mouvementUtil[2];
         memcpy(solutionVoisine,solutionCourante,instance->objetNb*sizeof(int));
         int fbestvoisin = 0;
         for(int i=0;i<instance->objetNb;i++){ //Première boucle for qui permet de changer les 0 en 1
             if(solutionVoisine[i]==0){
-                //On met l'objet i dans le sac
-                solutionVoisine[i] = 1;
-
-                //Evaluation de la solution voisine
-                int resultat=directResultat(solutionVoisine,instance);
-                if(resultat > fbestvoisin && directFaisable(solutionVoisine,instance) == 1){
-                    memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
-                    fbestvoisin = resultat;
+                int estTabou = 0;
+                for(int itabou = 0;itabou<nbMouvements;itabou++){
+                    if(listeMouvements[itabou][0] == i && listeMouvements[itabou][1] == i){
+                        estTabou = 1;
+                        break;
+                    }
                 }
 
-                //On le retire du sac pour passer à une autre solution
-                solutionVoisine[i] = 0;
+                if(estTabou == 0 || aspi == 1){
+                    //On met l'objet i dans le sac
+                    solutionVoisine[i] = 1;
+
+                    //Evaluation de la solution voisine
+                    int resultat=directResultat(solutionVoisine,instance);
+
+                    if(estTabou == 0){
+                        if(resultat > fbestvoisin && directFaisable(solutionVoisine,instance) == 1){
+                            memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
+                            fbestvoisin = resultat;
+                            mouvementUtil[0] = i;
+                            mouvementUtil[1] = i;
+                        }
+                    }
+                    else{
+                        if(resultat > fbest && directFaisable(solutionVoisine,instance) == 1){
+                            memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
+                            fbestvoisin = resultat;
+                            mouvementUtil[0] = i;
+                            mouvementUtil[1] = i;
+                        }
+                    }
+
+                    //On le retire du sac pour passer à une autre solution
+                    solutionVoisine[i] = 0;
+                }
             }
         }
 
@@ -215,20 +238,43 @@ void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax){
             if(solutionVoisine[i] == 1){
                 for(int j=0;j<instance->objetNb;j++){
                     if(solutionVoisine[j] == 0){
+                        int estTabou = 0;
+                        for(int itabou = 0;itabou<nbMouvements;itabou++){
+                            if(listeMouvements[itabou][0] == i && listeMouvements[itabou][1] == j){
+                                estTabou = 1;
+                                break;
+                            }
+                        }
+
+                        if(estTabou == 0 || aspi == 1){
                         //Permutation des éléments
                         solutionVoisine[i] = 0;
                         solutionVoisine[j] = 1;
 
                         //Evaluation de la solution voisine
                         int resultat=directResultat(solutionVoisine,instance);
-                        if(resultat > fbestvoisin && directFaisable(solutionVoisine,instance) == 1){
-                            memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
-                            fbestvoisin = resultat;
+
+                        if(estTabou == 0){
+                            if(resultat > fbestvoisin && directFaisable(solutionVoisine,instance) == 1){
+                                memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
+                                fbestvoisin = resultat;
+                                mouvementUtil[0] = i;
+                                mouvementUtil[1] = j;
+                            }
+                        }
+                        else{
+                            if(resultat > fbest && directFaisable(solutionVoisine,instance) == 1){
+                                memcpy(solutionBestVoisine,solutionVoisine,instance->objetNb*sizeof(int));
+                                fbestvoisin = resultat;
+                                mouvementUtil[0] = i;
+                                mouvementUtil[1] = j;
+                            }
                         }
 
                         //Permutation des éléments
                         solutionVoisine[i] = 1;
                         solutionVoisine[j] = 0;
+                        }
 
                     }
                 }
@@ -237,12 +283,32 @@ void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax){
 
         fcourant = fbestvoisin;
         memcpy(solutionCourante,solutionBestVoisine,instance->objetNb*sizeof(int));
+
+        int estTabou = 0;
+        for(int itabou = 0;itabou<nbMouvements;itabou++){
+            if(listeMouvements[itabou][0] == mouvementUtil[0] && listeMouvements[itabou][1] == mouvementUtil[1]){
+                estTabou = 1;
+                break;
+            }
+        }
+        if(estTabou == 0)
+            nbMouvements++;
+
+        if(nbMouvements <= tailleListe){ //On teste si le nombre de mouvements est inférieur ou égal à la taille max de la liste
+            listeMouvements = realloc(listeMouvements,nbMouvements*sizeof(int*));
+        }
+        else{ //Sinon on décale de tableau pour stocker la dernière permutation
+            for (int i=0; i<tailleListe;i++)
+                listeMouvements[i] = listeMouvements[i+1];
+        }
+        listeMouvements[nbMouvements-1]=mouvementUtil;
+
         if(fcourant>fbest){
             fbest=fcourant;
             memcpy(solutionBest,solutionCourante,instance->objetNb*sizeof(int));
-            i=0;
+            nbIte=0;
         }
-        fprec = fcourant;
+        nbIte++;
     }
 
     //Copie de la solution dans le tableau de destination
@@ -252,6 +318,5 @@ void metaTabouDirecte(int* tab, Instance *instance, int nbIteMax){
     free(solutionBest);
     free(solutionVoisine);
     free(solutionBestVoisine);
-
-    nbIte++;
+    free(listeMouvements);
 }
